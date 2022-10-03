0224c3ffcb6bbfd68c8d0df41a6a4d0f8addb08b
unknown <daliadaniele2002@gmail.com>
2022-10-03 21:04:04 +0200

    M

---
+++ Solution/Product manager/Product manager/Hardware.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_manager
+{
+    class Hardware : Product
+    {
+        public Hardware(string name, double price, int purchasedPieces) : base(name, price, purchasedPieces)
+        {
+
+        }
+    }
+}
---
+++ Solution/Product manager/Product manager/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_manager
+{
+     abstract class Product
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+        public int purchasedPieces { get; set; }
+
+        public Product(string name, double price, int purchasedPieces)
+        {
+            this.name = name;
+            this.price = price;
+            this.purchasedPieces = purchasedPieces;
+        }
+    }
+}
